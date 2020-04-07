57d05c40f4c3f906730cc18fa1d2ac9a1eab4eb4
li0t <leonardo@tiralinea.com>
2020-04-07 13:45:00 -0400

    Add utils declaration file

---
+++ lib/utils.d.ts
@@ -0,0 +1,17 @@
+/**
+ * @types fxSolve/utils
+ */
+
+export {
+  isUndefinedVariableError,
+  isUnexpectedTypeError,
+  isFormulaSymbol,
+  isRefErrorSymbol,
+  isValErrorSymbol,
+  isVariableNode,
+  isConstantNode,
+  parseExpressionNode,
+  splitExpressionNodes,
+  cleanExpression,
+  getVariables
+} from './utils';
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,79 +1,75 @@
 /**
- * @module formulas/services/formulas/utils
+ * @module fxSolve/utils
  */
 
-import FORMULAS_CONSTS from './consts';
+import * as FORMULAS_CONSTS from './consts';
 import parse5 from 'parse5';
 import math from 'mathjs';
+
+import { Node } from './interfaces';
 
 const MATH_CONSTANTS = Object.keys(math);
 
 /**
  * Library returns anonymous error with the "Undefined symbol" string.
- * @param {String} err
  */
-export function isUndefinedVariableError(err) {
+export function isUndefinedVariableError(err: string): boolean {
   return /Undefined symbol/.test(err);
 }
 
 /**
  * Library returns anonymous error with the "Unexpected type (...)" or "Cannot convert (...)" strings.
- * @param {String} err
  */
-export function isUnexpectedTypeError(err) {
+export function isUnexpectedTypeError(err: string): boolean {
   return /Unexpected type/.test(err) || /Cannot convert/.test(err);
 }
 
 /**
  * Checks if a string represents a component custom symbol.
- * @param {String} val
  */
-export function isFormulaSymbol(val) {
-  return !!Object.values(FORMULAS_CONSTS).find(symbol => val === symbol);
+export function isFormulaSymbol(val: string): boolean {
+  return !!Object.values(FORMULAS_CONSTS).find((symbol) => val === symbol);
 }
 
 /**
  * Checks if given value is a missing reference symbol
- * @param {*} val
  */
-export function isRefErrorSymbol(val) {
+export function isRefErrorSymbol(val: string): boolean {
   const re = new RegExp(FORMULAS_CONSTS.REF_ERROR);
   return re.test(val);
 }
 
 /**
  * Checks if given value is a missing reference symbol
- * @param {*} val
  */
-export function isValErrorSymbol(val) {
+export function isValErrorSymbol(val: string): boolean {
   const re = new RegExp(FORMULAS_CONSTS.VAL_ERROR);
   return re.test(val);
 }
 
 /**
  * Checks if node represents a variable.
- * @param {Object} node A parsed expression node
  */
-export function isVariableNode(node) {
+export function isVariableNode(node: Node) {
   return node.classes.includes('math-symbol');
 }
 
 /**
  * Checks if node represents a constant.
- * @param {Object} node A parsed expression node
  */
-function isConstantNode(node) {
+export function isConstantNode(node: Node) {
   return node.classes.includes('math-number');
 }
 
 /**
  * Returns a formatted node
- * @private
  */
-function parseExpressionNode(node) {
-  const newNode = {
-    classes: node.attrs.find(a => a.name === 'class').value,
-    value: node.childNodes[0].value
+export function parseExpressionNode(node: Node): Node {
+  const newNode: Node = {
+    classes: node.attrs.find((attr) => attr.name === 'class').value,
+    value: node.childNodes[0].value,
+    childNodes: [],
+    attrs: []
   };
 
   if (isConstantNode(newNode)) {
@@ -85,9 +81,8 @@ function parseExpressionNode(node) {
 
 /**
  * Returns an expression parsed as an html nodes tree.
- * @param {String} expression
  */
-export function splitExpressionNodes(expression) {
+export function splitExpressionNodes(expression: string): Node[] {
   const exp = cleanExpression(expression);
 
   const nodesString = math.parse(exp).toHTML();
@@ -99,9 +94,8 @@ export function splitExpressionNodes(expression) {
 
 /**
  * Replaces commas on constants for periods, for a given expression.
- * @param {String} expression
  */
-export function cleanExpression(expression) {
+export function cleanExpression(expression): string {
   const match = /\d,\d/.exec(expression);
 
   if (!match) {
@@ -114,20 +108,20 @@ export function cleanExpression(expression) {
 
   return cleanExpression(newExp);
 }
+
 /**
  * Parses a math expression string and returns an array with its variables names
- * @param {String} expression The math expression
  */
-export function getVariables(expression) {
+export function getVariables(expression: string): object[] {
   const variables = [];
 
   const exp = cleanExpression(expression);
 
   const node = math.parse(exp);
 
-  node.traverse(n => {
+  node.traverse((n) => {
     if (n.type === 'SymbolNode' && !MATH_CONSTANTS.includes(n.name)) {
-      const alreadyAdded = variables.find(v => v === n.name);
+      const alreadyAdded = variables.find((v) => v === n.name);
 
       if (!alreadyAdded) {
         variables.push(n.name);
@@ -137,14 +131,3 @@ export function getVariables(expression) {
 
   return variables;
 }
-
-export default {
-  isUndefinedVariableError,
-  isUnexpectedTypeError,
-  splitExpressionNodes,
-  isValErrorSymbol,
-  isRefErrorSymbol,
-  isVariableNode,
-  isFormulaSymbol,
-  getVariables
-};
